52a70af3105367728342f80b50021d28d11d809a
Andreas Kogelbauer <andreas.kogelbauer@itdesign.at>
2023-07-26 09:43:04 +0200

    Changed var name

--- converter/fnv1aHash.go
+++ converter/fnv1aHash.go
@@ -12,10 +12,10 @@ import (
 //	fmt.Println(hash)
 //	// prints "712dc882"
 func Fnv1aHash(input string) string {
-	h := fnv.New32a()
-	_, _ = h.Write([]byte(input))
+	ha := fnv.New32a()
+	_, _ = ha.Write([]byte(input))
 	// Achtung! das 08 ist wichtig - muessen genau
 	// 8 chars sein z.B. hash := common.Fnv1aHash("uswesampol01")
 	// hat eine fuehrende 0!
-	return fmt.Sprintf("%08x", h.Sum32())
+	return fmt.Sprintf("%08x", ha.Sum32())
 }
