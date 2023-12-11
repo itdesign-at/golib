d1d6c445bd13039b358d0f4eb90bb5aa7fc47cf8
Werner Neunteufl <100945666+itd-neunteufl@users.noreply.github.com>
2023-12-11 17:42:01 +0100

    update comments and test

--- structuredLogging/structuredLogging.go
+++ structuredLogging/structuredLogging.go
@@ -4,7 +4,12 @@ import (
 	"encoding/json"
 	"io"
 	"log/slog"
+	"math"
 	"os"
+	"os/user"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/nats-io/nats.go"
 )
@@ -18,42 +23,102 @@ type SlogLogger struct {
 }
 
 // New takes a config map with its parameters to start
+// Example:
+//
+//	structuredLogging.New(map[string]any{
+//	    "nats":   "nats://127.0.0.1:4222",
+//	    "file":   "/tmp/do-log.log",
+//	    "STDERR": true,
+//	})
+//
+// In its simplest form structuredLogging.New(nil) logs to STDERR
 func New(config map[string]any) *SlogLogger {
 	var sl SlogLogger
+
+	if config == nil {
+		config = map[string]any{"STDERR": true}
+	}
 	opt := &slog.HandlerOptions{
 		AddSource:   false,
 		Level:       slog.LevelDebug,
 		ReplaceAttr: nil,
 	}
-	if config == nil {
-		jh := slog.NewJSONHandler(os.Stderr, opt)
-		logger := slog.New(jh)
-		slog.SetDefault(logger)
-		return &sl
+
+	if n, ok := config["nats"].(string); ok && n != "" {
+		// nats reconnect documentation see
+		// https://docs.nats.io/using-nats/developer/connecting/reconnect
+		sl.natsHandler, _ = nats.Connect(
+			n,
+			nats.RetryOnFailedConnect(true),
+			nats.MaxReconnects(math.MaxInt),
+		)
 	}
+	if f, ok := config["file"].(string); ok && f != "" {
+		sl.fileHandler, _ = os.OpenFile(f, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	}
+	if b, ok := config["STDERR"].(bool); ok && b {
+		sl.stderrHandler = os.Stderr
+	}
+
+	jh := slog.NewJSONHandler(sl, opt)
+	logger := slog.New(jh)
+	slog.SetDefault(logger)
+
 	return &sl
 }
 
+// GenerateLogfileName generates a filename with a golang
+// time layout as input. "user.Current" is replaced with the username
+// of the user.Current() method.
+//
+// Example:
+//
+//	/var/log/messenger-user.Current-2006-01.log
+//
+// returns
+//
+//	/var/log/messenger-root-2023-12.log
+func GenerateLogfileName(layout string) string {
+	var str string
+
+	var username string
+	u, err := user.Current()
+	if err == nil {
+		username = u.Username
+		if username == "" {
+			username = u.Name
+		}
+		if username == "" {
+			username = u.Uid
+		}
+	} else {
+		uid := os.Getuid()
+		username = strconv.Itoa(uid)
+	}
+
+	layout = strings.Replace(layout, "user.Current", username, -1)
+	str = time.Now().Format(layout)
+
+	return str
+}
+
+// Write to all configured log handlers
 func (sl SlogLogger) Write(p []byte) (int, error) {
 	var data = make([]byte, len(p))
 	copy(data, p)
 	if sl.natsHandler != nil {
-		go func(b []byte) {
-			var subject string
-			var x map[string]any
-			_ = json.Unmarshal(b, &x)
-			if level, ok := x["level"].(string); ok {
-				subject = "slog." + level
-			} else {
-				subject = "slog.UNKNOWN"
-			}
-			_ = sl.natsHandler.Publish(subject, b)
-		}(data)
+		var subject string
+		var x map[string]any
+		_ = json.Unmarshal(data, &x)
+		if level, ok := x["level"].(string); ok {
+			subject = "slog." + level
+		} else {
+			subject = "slog.UNKNOWN"
+		}
+		_ = sl.natsHandler.Publish(subject, data)
 	}
 	if sl.fileHandler != nil {
-		go func(b []byte) {
-			_, _ = sl.fileHandler.Write(b)
-		}(data)
+		_, _ = sl.fileHandler.Write(data)
 	}
 	if sl.stderrHandler != nil {
 		_, _ = sl.stderrHandler.Write(data)
--- structuredLogging/structuredLogging_test.go
+++ structuredLogging/structuredLogging_test.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -19,11 +20,21 @@ func Test_StderrLogging(t *testing.T) {
 
 func Test_AllChannels(t *testing.T) {
 	New(map[string]any{
-		"nats":   "witest.itdesign.at:4222",
-		"file":   "/var/log/do-log.log",
-		"stderr": "yes",
+		"nats":   "nats://127.0.0.1:4222",
+		"file":   "/tmp/do-log.log",
+		"STDERR": true,
 	})
 	log.Println("Hallo World")
 	slog.Error("this is my first error")
 	slog.Debug("this is my first debug entry")
+}
+
+func TestGenerateLogfileName(t *testing.T) {
+	fileName := GenerateLogfileName("/var/log/messenger-user.Current-2006-01.log")
+	if strings.Contains(fileName, "user.Current") {
+		t.Errorf("wrong filename %q", fileName)
+	}
+	if strings.Contains(fileName, "2006") {
+		t.Errorf("wrong filename %q", fileName)
+	}
 }
